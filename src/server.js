fb2bcc63a1547df34f34086048b99dbb9af47e7a
464david464 <davidmenachem464@gmail.com>
2022-06-17 02:55:30 +0300

    try

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const path = require('path')
 const app = express()
 const viewPath = path.join(__dirname + '/../views/')
-const homeRouter = require('../routes/home.router')
+const homeRouter = require('../routes/home.router');
+const studentRouter = require('../routes/students.router');
 
 app.use((req, res, next) => {
     const { url, method } = req
-    console.log(`got req from ${url} | ${method}`);
+    console.log(`got req from ${url} | method ${method}`);
     next()
 })
 
+
+app.use('/students', studentRouter)
 app.use('/', homeRouter)
 
 app.listen(1212, () => {
